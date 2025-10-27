a1e7711292bdc8f325dfb72c9fe982e621852052
laszlo-tw <karanina@ucsb.edu>
2025-10-27 13:31:13 -0700

    lz - implemented basic routing (needs a lot of clean up)

--- TODO.md
+++ TODO.md
@@ -1,7 +1,8 @@
 - [ ] update README.md
 - [x] implement view/edit pack page
 - [x] implement pack collection page
-- [ ] implement routing: home page = CollectionView, clicking on a deck name routes to DeckView, clicking the Begin Practice button routes to Practice
+- [x] implement routing: home page = CollectionView, clicking on a deck name routes to DeckView, clicking the Begin Practice button routes to Practice
+- [ ] improve routing: back arrows
 - [ ] styling, of course
 
 additional features and functionalities have been brainstormed in my notebook.
--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,9 +1,21 @@
 import { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
 import { Flashcard, Deck } from "../types";
+import my_decks from '../data/My_Decks';
 // import Header from "./Header";
 // import Card from "./Card";
 
-const Practice: React.FC<Deck> = ({name, cards}) => {
+// const Practice: React.FC<Deck> = ({name, cards}) => {
+const Practice: React.FC = () => {
+    // routing:
+    const { deckID } = useParams();
+    const deck = my_decks.decks.find(d => d.id === Number(deckID));
+    // if (!deck) return <div>Deck not found.</div> // need to improve
+
+
+    // everything else:
+
     // const [side, setSide] = useState<boolean>(true);
     const [index, setIndex] = useState<number>(0);
     const [faceUp, setFaceUp] = useState<boolean>(true);
@@ -14,7 +26,7 @@ const Practice: React.FC<Deck> = ({name, cards}) => {
     
     const previous = () => {
         if (index == 0) {
-            setIndex(cards.length - 1);
+            setIndex(deck ? deck.cards.length - 1 : 0);
         } else {
             setIndex(index - 1);
         }
@@ -22,27 +34,29 @@ const Practice: React.FC<Deck> = ({name, cards}) => {
     };
 
     const next = () => {
-        setIndex((index+1)%cards.length)
+        setIndex(deck? (index+1)%deck.cards.length : 0)
         setFaceUp(true); // display front
     };
     
     return (
-        <div className="practice">
-            {/* <Header title={name}/> */}
-            <header>
-                <h1>{name}</h1>
-            </header>
-            <div className='practice-layout'>
-                <div className="practice-side-panel" onClick={previous}><h2>⟨</h2></div>
-                <div className="practice-center-panel">
-                    {/* <Card card={cards[index]} side={true}/> */}
-                    <div className="card" onClick={() => flip()}>
-                        <p className='card-text'>{faceUp ? cards[index].front : cards[index].back }</p>
+        deck ?
+            <div className="practice">
+                {/* <Header title={name}/> */}
+                <header>
+                    <h1>{deck.name}</h1>
+                </header>
+                <div className='practice-layout'>
+                    <div className="practice-side-panel" onClick={previous}><h2>⟨</h2></div>
+                    <div className="practice-center-panel">
+                        {/* <Card card={cards[index]} side={true}/> */}
+                        <div className="card" onClick={() => flip()}>
+                            <p className='card-text'>{faceUp ? deck.cards[index].front : deck.cards[index].back }</p>
+                        </div>
                     </div>
+                    <div className="practice-side-panel" onClick={next}><h2>⟩</h2></div>
                 </div>
-                <div className="practice-side-panel" onClick={next}><h2>⟩</h2></div>
             </div>
-        </div>
+        : <div>Deck not found.</div>
     );
 };
 
